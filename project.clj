b0bd583f59a5229a98f7b2d8dd189eefed177e82
SgtZdog <kevin@zemon.name>
2019-11-07 12:54:16 -0600

    [MAN-1720]

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :dependencies [
     [leiningen-core "2.7.0"]
     [ns-tracker "0.3.1"]
-    [org.clojure/clojure "1.10.0"]])
+    [org.clojure/clojure "1.10.0"]]
+  :deploy-repositories [["releases" {:url "https://mvn.redlion.net/repository/maven-releases"
+                                     :sign-releases false}]
+                        ["snapshots" {:url "https://mvn.redlion.net/repository/maven-snapshots"
+                                      :sign-releases false}]])
